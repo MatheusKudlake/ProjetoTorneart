acb904bacd2a8f0356cc61c5d2f87f238da3622e
MatheusKudlake <178607990+MatheusKudlake@users.noreply.github.com>
2026-01-07 09:36:48 -0300

    📝 tradução do select de pesquisa

--- assets/js/dselect.js
+++ assets/js/dselect.js
@@ -103,7 +103,7 @@ function dselect(el, option = {}) {
   size = size !== '' ? ` form-select-${size}` : ''
   const classToggler = `form-select${size}`
 
-  const searchInput = search ? `<input onkeydown="return event.key !== 'Enter'" onkeyup="dselectSearch(event, this, '${classElement}', '${classToggler}', ${creatable})" type="text" class="form-control" placeholder="Search" autofocus>` : ''
+  const searchInput = search ? `<input onkeydown="return event.key !== 'Enter'" onkeyup="dselectSearch(event, this, '${classElement}', '${classToggler}', ${creatable})" type="text" class="form-control" placeholder="Pesquisar" autofocus>` : ''
 
   function attrBool(attr) {
     const attribute = `data-dselect-${attr}`
@@ -199,7 +199,7 @@ function dselect(el, option = {}) {
           <div class="dselect-items" style="max-height:${maxHeight};overflow:auto">
             ${itemTags(el.querySelectorAll('*'))}
           </div>
-          <div class="${classNoResults} d-none">No results found</div>
+          <div class="${classNoResults} d-none">Sem resultados</div>
         </div>
       </div>
       ${clearBtn}
